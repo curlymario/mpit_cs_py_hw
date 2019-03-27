c36d0385b150998b43e36d2c325b15bb4a928da4
Maxim Sivokon <maxim@sivokon.me>
2019-03-28 02:05:40 +0300

    lab 11

--- dynamic.py
+++ dynamic.py
@@ -13,9 +13,9 @@ def fib_light(n):
     return right if n > 0 else left
 
 # из лекции с занятием памяти
-fib = [0, 1] + [0]*(n - 1)
-for i in range (2, n+1):
-    fib[i] = fib[i -1] + fib[i - 2]
+# fib = [0, 1] + [0]*(n - 1)
+# for i in range (2, n+1):
+#     fib[i] = fib[i -1] + fib[i - 2]
 # return fib[n]
  
 def count_trajectories(N):
@@ -24,7 +24,7 @@ def count_trajectories(N):
     """ 
     K = [0, 1] + [0]*(N-1) # у лектора было [0]*N
     for i in range(2, N+1):
-        K[i] = K[i - 2] + K[i -1]
+        K[i] = K[i - 2] + K[i - 1]
     return K[N]
 
 def count_allowed_trajectories(N:int, allowed:list):
@@ -48,3 +48,58 @@ def count_min_cost(N:int, price:list):
     for i in range(3, N+1):
         cost[i] = price[i] + min(cost[i-1], cost[i-2])
     return cost[N]
+
+
+# == Лабораторная ==
+
+def count_trajectories_3(N):
+    """ Кузнечик имеет два варианта хода: +1, +2, +3
+        Функция возвращает, сколько есть траекторий пути от 1 до N
+    """ 
+    K = [0, 1, 1] + [0]*(N-2) 
+    for i in range(3, N+1):
+        K[i] = K[i - 3] + K[i - 2] + K[i - 1]
+    return K[N]
+
+def count_trajectories_3(N):
+    """ Кузнечик имеет два варианта хода: +1, +2, *3
+        Функция возвращает, сколько есть траекторий пути от 1 до N
+    """ 
+    K = [0, 1, 1, 3] + [0]*(N-3) 
+    for i in range(4, N+1):
+        if i//3 == 0:
+            K[i] = K[i/3] + K[i - 2] + K[i - 1]
+        else:
+            K[i] = K[i - 2] + K[i - 1]
+    return K[N]
+
+def calculate_min_cost(n, price):
+    C = [0, price[1], price[1]+price[2]] + [0]*(n-2)
+    for i in range(3, n+1):
+        C[i] = min(C[i-1], C[i-2]) + price[i]
+    return C[n]
+
+def find_min_cost_route(n, price):
+    C = [0, price[1], price[1]+price[2]] + [0]*(n-2)
+    prev = [0, 1, 1]
+    for i in range(3, n+1):
+        if C[i-1] <= C[i-2]:
+            C[i] = C[i-1] + price[i]
+            prev.append(i - 1)
+        else:   # C[i-2] < C[i-1]
+            C[i] = C[i-2] + price[i]
+            prev.append(i - 2)
+
+    k = prev[-1]
+    path = [n] + [k]
+    while prev[k] != 1:
+        path.append(prev[k])
+        k = prev[k]
+    path.append(1)
+    path[:] = path[::-1]
+    return(path)
+
+# price = [1, 2, 4, 3, 2, 5, 6, 7, 3, 7, 8, 2, 8, 9, 1, 3, 2]
+# n = len(price) - 1
+# path = find_min_cost_route(n, price)
+# print(path)
