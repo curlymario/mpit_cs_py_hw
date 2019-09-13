809bd4da22fb2dc4d1f1637c51fd1501882517b9
curlymario <maxim@sivokon.me>
2019-09-13 15:01:55 +0300

    main logic

--- lect_16/9_languages.py
+++ lect_16/9_languages.py
@@ -19,12 +19,27 @@ def import_list(file_path) -> dict:
     return datalist
 
 
-def find_countries(language, datalist) -> str:
+def find_countries(requested_language, datalist) -> str:
     """ Получает имя языка (str)
         Ищем совпадения среди значений словаря
-        Выдаёт страну(ы) (str)
+        Выдаёт страну(ы) (list)
     """
-    pass
+    result_countries = []
+    for country, languages in datalist.items():
+        if requested_language in languages:
+            result_countries.append(country)
+    return result_countries
+
+def input_int(user_input) -> str:
+    """ Проверяет, является ли введённый текст числом и преобразует его в int"""
+    while True:
+        try:
+            integer_input = int(user_input)
+            break
+        except ValueError as e:
+            print("You should fisrt enter number, please try again")
+            print(e)
+    return integer_input
 
 def get_lang_list(datalist):
     """ Пользовательский ввод языков:
@@ -32,8 +47,17 @@ def get_lang_list(datalist):
         Получает поочерёдно названия языков
         Выводит список стран согласно словарю
     """
-    pass
+    list_len = input_int(input("Пожалуйста, введите число интересующих языков: "))
+    for i in range(list_len):
+        if i == 0:
+            requested_lang = input("Пожалуйста, введите интересующий вас язык\n")
+        else:
+            requested_lang = input("Пожалуйста, введите следующий язык\n")
+        result_countries = find_countries(requested_lang, datalist)
+        print(' '.join(result_countries))
+    print("Поиск завершён")
+
 
 if __name__ == "__main__":
-    countries_list = import_list('http://judge.mipt.ru/mipt_cs_on_python3/extra/lab17/task5/input.txt')
-    print(countries_list)
+    country_lang_list = import_list('http://judge.mipt.ru/mipt_cs_on_python3/extra/lab17/task5/input.txt')
+    get_lang_list(country_lang_list)
