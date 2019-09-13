2314e086177ae59e3b310fd413265703e2f21d2d
curlymario <maxim@sivokon.me>
2019-09-13 13:39:41 +0300

    complete file unpacking function

--- lect_16/9_languages.py
+++ lect_16/9_languages.py
@@ -1,24 +1,39 @@
 # ========== Страны и языки ===========
 
-def import_dict(file_path) -> dict:
+import requests
+
+def import_list(file_path) -> dict:
     """ Вводим текст из удалённого файла (ссылка на файл, str)
         Выдаём словарь (dict):
         Ключ: страна (str)
         Значение: языки (str или tuple(str))
     """
-    pass
+    file = requests.get(file_path)
+    datalist = dict()
+    for line in file.iter_lines(decode_unicode=True):
+        dict_entry = line.split(" : ")
+        country = dict_entry[0]
+        language = (*dict_entry[1].split(" "), )
+        datalist[country] = language
 
-def find_countries(language, dictionary) -> str:
+    return datalist
+
+
+def find_countries(language, datalist) -> str:
     """ Получает имя языка (str)
         Ищем совпадения среди значений словаря
         Выдаёт страну(ы) (str)
     """
     pass
 
-def get_lang_list(dictionary):
+def get_lang_list(datalist):
     """ Пользовательский ввод языков:
         Получает размер списка
         Получает поочерёдно названия языков
         Выводит список стран согласно словарю
     """
     pass
+
+if __name__ == "__main__":
+    countries_list = import_list('http://judge.mipt.ru/mipt_cs_on_python3/extra/lab17/task5/input.txt')
+    print(countries_list)
