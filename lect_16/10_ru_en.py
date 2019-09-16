6cd8c16f4ee987787d2f1c952ec6a9ecc02afabd
curlymario <maxim@sivokon.me>
2019-09-16 17:13:22 +0300

    finished the task (it was suspiciously fast)

--- lect_16/10_ru_en.py
+++ lect_16/10_ru_en.py
@@ -17,8 +17,27 @@ def import_dict(file_path) -> dict:
 
 def reverse_dict(original_dict):
     """ Получает словарь, выдаёт обратный словарь """
-    pass
+    result_dict = dict()
+    for original_word, translation in original_dict.items():
+        for translated_word in translation.split(", "):
+            if translated_word in result_dict:
+                result_dict[translated_word].append(original_word)
+                result_dict[translated_word].sort()
+            else:
+                result_dict[translated_word] = [original_word, ]
+    return result_dict
 
-def export_dict(dictionary):
+def export_dict(dictionary, target_file):
     """ Получает словарь, сортирует и сохраняет в файл """
-    pass
+    dict_lines = ["\t-\t".join((key, ", ".join(value))) for key, value in dictionary.items()]
+    dict_lines.sort()
+    file = open(target_file, "w+", encoding="utf8")
+    for line in dict_lines:
+        file.write(line + "\n")
+    file.close()
+    return dict_lines
+
+if __name__ == "__main__":
+    en_ru = import_dict('http://judge.mipt.ru/mipt_cs_on_python3/extra/lab17/task6/en-ru.txt')
+    ru_en = reverse_dict(en_ru)
+    export_dict(ru_en, "ru-en.txt")
