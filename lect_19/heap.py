19043f178bd9572c0b38160e1bb64545110248a9
curlymario <maxim@sivokon.me>
2019-10-11 17:25:47 +0300

    lecture 19 pt 2

--- lect_19/heap.py
+++ lect_19/heap.py
@@ -1,3 +1,5 @@
+# ===== === Куча === ========
+
 """
 Дерево — однонаправленный граф
 Дерево в котором у каждого родителя не более двух прямых потомков — двоичное дерево
@@ -60,3 +62,56 @@ Class Heap:
             if i == j:
                 break
             self.values[i], self.values[j] = self.values[j], self.values[i]
+
+"""
+Асимптотика кучи
+shift_up()      O(logN)
+shift_down      O(logN)
+min             O(1)
+extract_min     O(logN)
+insert          O(logN)
+"""
+
+# ===== Сортировка кучей ========
+
+"""
+Кладём массив в кучу
+Извлекаем из кучи новый массив c помощью extract_min
+"""
+
+def heapify(array):
+    heap = Heap()
+    for item in array:      # O(N)
+        heap.insert(item)   # O(log N)
+    return heap             # O(N log N)
+
+def get_sorted_array(heap):
+    array = []
+    while heap.size:                        # O(N)
+        array.append(heap.extract_min())    # O(log N)
+    return array                            # O(N log N)
+
+def heapsort(array):
+    return get_sorted_array(heapify(array))
+
+"""
+heapify()               O(N*logN)
+get_sorted_array()      O(N*logN)
+heapsort()              O(N*logN)
+
+число элементов, которые имеют хотя бы одного потомка = N/2
+можно сделать heapify быстрее, не вводя элементы по одному и делая shift_down,
+а разом введя все элементы и сделав shift_down только узлам с потомками
+"""
+
+def heapify_fast(array):
+    heap = Heap()
+    heap.values = array[:]
+    heap.size = len(array)
+    for i in reversed(range(n//2)):
+        heap.shift_down(i)
+    return heap
+"""
+лектор показал какие-то сложные математические рассчёты, в результате которых получается O(2N)
+heapify_fast()          O(N)
+"""
